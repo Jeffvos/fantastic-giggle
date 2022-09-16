ea8894c49fb3c95f1ce638ffdb1ad2997f1a782b
Jeffrey <jeff.vos@aol.com>
2022-09-16 17:55:48 +0200

    adding s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -140,3 +140,16 @@ resource "aws_instance" "web" {
     "Terraform" = "true"
   }
 }
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "s3_bucket_for_data"
+  tags = {
+    Name = "s3_bucket"
+    Purpose = "sample code"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl = "private"
+}
