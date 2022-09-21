c28c879827e86caa169675e2e68026bc0e898efc
Jeffrey <jeff.vos@aol.com>
2022-09-21 13:12:53 +0200

    added public url and vpc info

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,13 @@ output "vpc_id" {
     description = "vpc id output"
     value = aws_vpc.vpc.id
 }
+
+output "public_url" {
+    description = "webservers public url"
+    value = "https://${aws_instance.web_server.private_ip}:8080/index.html"
+}
+
+output "vpc_info" {
+  description = "vpc information"
+  value = "${aws_vpc.vpc.tags.Environment} vpc has an id of ${aws_vpc.vpc.id}"
+}
