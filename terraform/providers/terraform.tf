e22601b5a12ce475fcbf1d5d1d515f17d7878a48
Jeffrey <jeff.vos@aol.com>
2022-09-22 11:46:00 +0200

    http provider

---
+++ terraform/providers/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">=1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+  }
+}
