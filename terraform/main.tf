e1d047330de4d03590c3ebcc288a80481a0381a3
Jeffrey <jeff.vos@aol.com>
2022-09-24 08:35:07 +0200

    adding subnet 1 module server outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -413,3 +413,11 @@ module "server_subnet_1" {
     aws_security_group.vpc-web.id
   ]
 }
+
+output "publice_ip_server_subnet_1" {
+  value = module.server_subnet_1.public_ip
+}
+
+output "public_dns_server_subnet_1" {
+  value = module.server_subnet_1.public_dns
+}
