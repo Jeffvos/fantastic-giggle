6cde33a22ea903ec7e728ad4f299a9f60473ee88
Jeffrey <jeff.vos@aol.com>
2022-09-27 16:07:30 +0200

    vpc by module

--- terraform/main.tf
+++ terraform/main.tf
@@ -458,3 +458,20 @@ output "s3-bucket-name" {
   value = module.s3-bucket.s3_bucket_bucket_domain_name
 
 }
+
+module "vpc" {
+  source             = "terraform-aws-modules/vpc/aws"
+  name               = "vpc-tf"
+  version            = "3.16.0"
+  cidr               = "10.0.0.0/16"
+  azs                = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  tags = {
+    Name        = "VPC from Module"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
