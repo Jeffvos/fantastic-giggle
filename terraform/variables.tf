0543187e5a013a29d5ecbf5ae9bef7486cc3a650
jeffvos <jeff.vos@aol.com>
2022-10-04 09:36:32 +0200

    added us east 1 availiblity zones

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -49,3 +49,15 @@ variable "environment" {
   type    = string
   default = "PROD"
 }
+
+variable "us-east-1-azs" {
+  type = list(string)
+  default = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c",
+    "us-east-1d",
+    "us-east-1e"
+  ]
+
+}
