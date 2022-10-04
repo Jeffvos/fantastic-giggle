5718760712fb3c4cdc31f70062f7465c75ada289
jeffvos <jeff.vos@aol.com>
2022-10-04 09:43:46 +0200

    ip map

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -59,5 +59,12 @@ variable "us-east-1-azs" {
     "us-east-1d",
     "us-east-1e"
   ]
+}
 
+variable "ip" {
+  type = map(string)
+  default = {
+    prod = "10.0.150.0/24"
+    dev = "10.0.250.0/24"
+  }
 }
